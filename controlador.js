85af515938de5fde6d32423583fbd45e8f9639dc
juan jose <j>
2022-04-27 17:20:56 -0500

    clinica del play mia

--- controlador.js
+++ controlador.js
@@ -1,9 +1,88 @@
-let producto={
+
+let producto1={
     nombre:"PS4",
     capacidad:"1TB",
-    foto:"Nan",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/play%204.jpg?alt=media&token=49ad9283-7b59-4c06-968a-5efa4252c2bf",
     estado:"Segunda",
     precio:500000
 }
-console.log(producto)
-console.log(producto.nombre)
+console.log(producto1)
+console.log(producto1.nombre)
+
+let producto2={
+    nombre:"mando ps5",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/mando%20play5.jpg?alt=media&token=e4cc46fe-f15a-46bc-87e5-cac02842ec22",
+    estado:"Nuevo",
+    precio:300000
+}
+console.log(producto2)
+console.log(producto2.nombre)
+
+let producto3={
+    nombre:"mando ps4",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/mando%20play%204.jfif?alt=media&token=6f34fbda-9605-4560-ac10-b02f55932381",
+    estado:"Nuevo",
+    precio:90000
+}
+console.log(producto3)
+console.log(producto3.nombre)
+
+let producto4={
+    nombre:"Xbox protector",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/Xbox%20mando%20protector.jfif?alt=media&token=d1ba60db-8ed9-447c-a39c-b363de4a1e4e",
+    estado:"Nuevo",
+    precio:20000
+}
+console.log(producto4)
+console.log(producto4.nombre)
+
+let producto5={
+    nombre:"Xbox mando",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/Xbox%20mando.jfif?alt=media&token=582b6fe0-3546-4e4a-ba64-816e72b8af35",
+    estado:"Nuevo",
+    precio:320000
+}
+console.log(producto5)
+console.log(producto5.nombre)
+
+let producto6={
+    nombre:"cargador ps4",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/base%20carga%20mando%20ps4.jpg?alt=media&token=00dcdbcc-95e1-434a-a4d5-4a31a27d60f1",
+    estado:"Nuevo",
+    precio:86000
+}
+console.log(producto6)
+console.log(producto6.nombre)
+
+let producto7={
+    nombre:"Base nintendo switch",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/base%20nintendo%20switch.jfif?alt=media&token=bd9126fe-98ed-4dfb-a639-b82410fcd430",
+    estado:"Nuevo",
+    precio:86000
+}
+console.log(producto7)
+console.log(producto7.nombre)
+
+let producto8={
+    nombre:"Base nintendo switch",
+    foto:"https://firebasestorage.googleapis.com/v0/b/afks-150ef.appspot.com/o/base%20nintendo%20switch.jfif?alt=media&token=bd9126fe-98ed-4dfb-a639-b82410fcd430",
+    estado:"Nuevo",
+    precio:86000
+}
+console.log(producto7)
+console.log(producto7.nombre)
+
+let productos=Array(
+    producto2,
+    producto3,
+    producto4,
+    producto5,
+    producto6,
+    
+)
+console.log(productos)
+
+productos.forEach(funtion(producto){
+    console.log(producto)
+    console.log(producto.nombre)
+})
